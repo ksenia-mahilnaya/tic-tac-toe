772b515d382d2c75cc6983d36a89e850db8f41cf
ksenia-mahilnaya <ksenia.vlady@gmail.com>
2017-02-05 22:06:50 +0200

    tic-tac-toe: fixed mistakes

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -38,74 +38,52 @@ class TicTacToe {
 
     isFinished() {
 
-        if ((this.getWinner() !== null) || (this.isDraw())) {
+        if ((this.getWinner()) || (this.isDraw())) {
           return true;
         }
         return false;
     }
 
     getWinner() {
-        var winnerExists = false;
 
         for (var i = 0; i < 3; i++) {
           if ((this.matrix[i][0] === this.matrix[i][1]) && (this.matrix[i][1] === this.matrix[i][2]) && (this.matrix[i][0] !== null)) {
           return this.matrix[i][0];
-          winnerExists = true;
-          break;
           }
         }
 
         for (var j = 0; j < 3; j++) {
           if ((this.matrix[0][j] === this.matrix[1][j]) && (this.matrix[1][j] === this.matrix[2][j]) && (this.matrix[2][j] !== null)) {
           return this.matrix[0][j];
-          winnerExists = true;
-          break;
           }
         }
 
-          if ((this.matrix[0][0] === this.matrix[1][1]) && (this.matrix[1][1]  === this.matrix[2][2]) && (this.matrix[2][2] !== null)) {
+        if ((this.matrix[0][0] === this.matrix[1][1]) && (this.matrix[1][1]  === this.matrix[2][2]) && (this.matrix[2][2] !== null)) {
           return this.matrix[0][0];
-          winnerExists = true;
         }
 
         if ((this.matrix[2][0] === this.matrix[1][1]) && (this.matrix[1][1]  === this.matrix[0][2]) && (this.matrix[0][2] !== null)) {
-        return this.matrix[2][0];
-        winnerExists = true;
+          return this.matrix[2][0];
         }
 
-        if (!winnerExists) {
-          return null;
-        }
+        return null;
+
     }
 
     noMoreTurns() {
-        loop:
         for (var i = 0; i < 3; i++) {
           for (var j = 0; j < 3; j++) {
-
             if (this.matrix[i][j] === null ) {
-
               return false;
-              break loop;
             }
           }
         }
         return true;
-      }
+    }
 
     isDraw() {
-        var noEmptyCells = true;
 
-        for (var i = 0; i < 3; i++) {
-          for (var j = 0; j < 3; j++) {
-            if (this.matrix[i][j] === null) {
-                noEmptyCells = false;
-                break;
-              }
-            }
-          }
-
-        if ((this.getWinner() === null) && (noEmptyCells)) {
+        if ((!this.getWinner()) && (this.noMoreTurns())) {
             return true;
         }
         return false;
